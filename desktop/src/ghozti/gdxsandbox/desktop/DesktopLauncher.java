c5bd76515a92163826acaa5c2cbb493539c85148
Ghozti <62896836+Ghozti@users.noreply.github.com>
2022-09-25 17:53:00 -0400

    Initial commit

---
+++ desktop/src/ghozti/gdxsandbox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ghozti.gdxsandbox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ghozti.gdxsandbox.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
